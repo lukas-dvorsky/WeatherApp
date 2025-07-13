9004a2d58934e9dbe3d32975f3cc0e627487fb7b
lukas-dvorsky <dvorskejlukas3333@gmail.com>
2025-07-13 18:28:35 +0200

    Reading data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Whisperer from './components/Whisperer/Whisperer'
-import citiesData from '../public/city.list.json'
+import citiesData from './assets/city.list.json'
 import type { City } from './utils/types'
 import { useDispatch, useSelector } from 'react-redux';
 import { setSelectedCity } from './redux/state/citySlice';
@@ -12,7 +12,33 @@ function App() {
 
   const selectedCity = useSelector((state: RootState) => state.city.selectedCity)
 
-  function handleClick(city: City) {
+  async function handleClick(city: City): Promise<void> {
+    // api call
+    const apiKey = import.meta.env.VITE_API_KEY
+
+    const cityName = city.name;
+    const countryCode = city.country;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${countryCode}&appid=${apiKey}`;
+
+    try {
+      if(!apiKey) {throw new Error("API key is missing")}
+        
+      const response  = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Error: Couldnt find city.")
+        }
+
+        const json = await response.json();
+        console.log(json);
+    } catch (error: unknown) {
+      if(error instanceof Error) {
+        console.error(error.message);
+      } else {
+        console.error("Unknown error occured.")
+      }
+    }
+
+
     dispatch(setSelectedCity(city));
   }
 
